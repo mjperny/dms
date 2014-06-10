1136ae604a5484d7b2c72d2d37d56de0a0e3354d
mgoebel-tgm <schueler@debian>
2014-06-10 19:36:42 +0200

    login with errors (Symbol not found)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,6 +5,7 @@ import play.*;
 import play.db.jpa.*;
 import play.mvc.*;
 import views.html.*;
+import static play.data.Form.*;
 
 public class Application extends Controller {
 
@@ -12,8 +13,8 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
     public static Result login() {
-        return ok(
-          login.render()
+       return ok(
+            login.render(form(Login.class))
         );
     }
     public static Result createDocument() {
@@ -30,6 +31,30 @@ public class Application extends Controller {
 public static class CreateDocument {
     public String name;
     public String comment;
+
+}
+public static Result authenticate() {
+    Form<Login> loginForm = form(Login.class).bindFromRequest();
+    if (loginForm.hasErrors()) {
+        return badRequest(login.render(loginForm));
+    } else {
+        session().clear();
+        session("name", loginForm.get().name);
+        return redirect(
+            routes.Application.index()
+        );
+    }
+}
+
+public String validate() {
+    if (User.authenticate(name, password) == null) {
+      return "Invalid user or password";
+    }
+    return null;
+}
+public static class Login {
+
+    public String name;
 
 }
 }
